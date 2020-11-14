fcd16511ef9538df4db3ac0f87033d8800428d18
Arsipa <senya.suvorov.2003@mail.ru>
2020-11-15 01:08:06 +0300

    Delete search, Materials.php, Copyright center

--- Template/header.php
+++ Template/header.php
@@ -77,14 +77,6 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-9">
-						<div class="navbar-header">
-							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-								<span class="sr-only">Toggle navigation</span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-								<span class="icon-bar"></span>
-							</button>
-						</div>
 						<div class="mainmenu pull-left">
 							<ul class="nav navbar-nav collapse navbar-collapse">
 								<li><a href="index.html" class="active">Главная</a></li>
@@ -93,11 +85,6 @@
 								<li><a href="#">Отзывы</a></li>
 								<li><a href="#">Контакты</a></li>
 							</ul>
-						</div>
-					</div>
-					<div class="col-sm-3">
-						<div class="search_box pull-right">
-							<input type="text" placeholder="Поиск..."/>
 						</div>
 					</div>
 				</div>
